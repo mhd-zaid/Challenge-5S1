df64374daf62bd5a3782e4e485e87ec9ac5a4549
dan1M <maneadaniel555@gmail.com>
2024-04-15 14:14:08 +0200

    init front

---
+++ client/src/main.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import extend_theme from './utils/chakra-theme.js';
+import './index.css';
+import { ProtectedRoute } from './layouts/ProtectedRoute.jsx';
+import NotFoundPage from '@/pages/NotFoundPage.jsx';
+import App from './App.jsx';
+import Home from './pages/HomePage';
+import Profile from './pages/ProfilePage';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
+import ForgetPasswordPage from './pages/auth/ForgetPasswordPage';
+import ResetPasswordPage from './pages/auth/ResetPasswordPage';
+import EmailVerifiedPage from './pages/auth/EmailVerifiedPage.jsx';
+
+const theme = extendTheme(extend_theme);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />}>
+            <Route index element={<Home />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path='profile' element={<Profile />} />
+            </Route>
+            <Route path='auth'>
+              <Route path='login' element={<LoginPage />} />
+              <Route path='register' element={<RegisterPage />} />
+              <Route path='forgetpassword' element={<ForgetPasswordPage />} />
+              <Route
+                path='resetpassword/:token'
+                element={<ResetPasswordPage />}
+              />
+              <Route path='verify' element={<EmailVerifiedPage />} />
+            </Route>
+            <Route path='*' element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  </React.StrictMode>
+);
