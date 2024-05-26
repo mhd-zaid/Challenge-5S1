0f7345ab2496e01fec46d20a2089a572264d8f05
Zaid MOUHAMAD <zaidmouhamad@gmail.com>
2024-05-26 21:40:30 +0200

    custom endpoint for distinct

--- server/src/Entity/Service.php
+++ server/src/Entity/Service.php
@@ -2,15 +2,62 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use App\Controller\ServiceController;
 use App\Repository\ServiceRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ServiceRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    normalizationContext: ['groups' => ['service:read']],
+    operations: [
+       new Post(),
+       new GetCollection(),
+       new GetCollection(
+            name: 'get_services_distinct_by_column',
+            uriTemplate: '/api/services/distinct/{column}',
+            controller: ServiceController::class,
+            requirements: [
+                'column' => '\w+',
+            ],
+            description: 'Get all services distinct by column',
+            paginationEnabled: false,
+            openapiContext: [
+                'parameters' => [
+                    [
+                        'name' => 'column',
+                        'in' => 'path',
+                        'required' => true,
+                        'schema' => [
+                            'type' => 'string',
+                            'enum' => [
+                                'id',
+                                'name',
+                                'description',
+                                'cost',
+                                'duration',
+                                'studio'
+                        ],
+                        'description' => 'The column to distinct. Allowed values: id, name, description, cost, duration, studio',
+                        'summary' => 'Retrieve distinct services by column'
+                        ],
+                    ],
+                ],
+            ],
+       ),
+       new Delete(),
+    ]
+)
+]
 class Service
 {
     use Traits\BlameableTrait;
@@ -19,21 +66,28 @@ class Service
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['service:read'])]
+    #[ApiProperty(identifier: false)]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['service:read'])]
     private ?string $name = null;
 
     #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['service:read'])]
     private ?string $description = null;
 
     #[ORM\Column]
+    #[Groups(['service:read'])]
     private ?int $cost = null;
 
     #[ORM\Column(type: Types::TIME_MUTABLE)]
+    #[Groups(['service:read'])]
     private ?\DateTimeInterface $duration = null;
 
     #[ORM\ManyToOne(inversedBy: 'services')]
+    #[Groups(['service:read'])]
     private ?Studio $studio = null;
 
     #[ORM\OneToMany(mappedBy: 'service', targetEntity: ServiceEmployee::class)]
