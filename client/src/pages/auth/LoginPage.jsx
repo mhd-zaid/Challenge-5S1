7149619d0ac47f527e3616315d54653ae8126729
jugo92 <jugzek92@outlook.fr>
2024-06-03 14:20:42 +0200

    css login page

--- client/src/pages/auth/LoginPage.jsx
+++ client/src/pages/auth/LoginPage.jsx
@@ -5,6 +5,8 @@ import {
   FormControl,
   FormLabel,
   Input,
+  InputGroup,
+  InputRightElement,
   Stack,
   Heading,
   Text,
@@ -12,17 +14,19 @@ import {
   Spinner
 } from '@chakra-ui/react';
 import { useParams, Link } from 'react-router-dom';
+import { BiShowAlt, BiHide  } from "react-icons/bi";
 
 
 import { useAuth } from '../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [error, setError] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [isResendingEmail, setIsResendingEmail] = useState(false); // Nouvel état pour suivre l'état de renvoi de l'e-mail
+  const [show, setShow] = useState(false);
   const toast = useToast();
   const navigate = useNavigate();
 
@@ -30,6 +34,7 @@ const LoginPage = () => {
 
   const handleLogin = async (e) => {
     e.preventDefault();
+    if(validForm()){
     setIsLoading(true);
     setError('');
     try {
@@ -39,7 +44,7 @@ const LoginPage = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          email: username,
+          email: email,
           password: password,
         }),
       });
@@ -57,24 +62,42 @@ const LoginPage = () => {
       } else {
         const errorData = await response.json();
         if (errorData.error === 'User is not validated') {
-          setError('Votre email n\'a pas encore été validé. Veuillez vérifier votre boîte de réception et valider votre compte.');
+          setError({
+            api:'Votre email n\'a pas encore été validé. Veuillez vérifier votre boîte de réception et valider votre compte.'
+        });
         } else {
-          setError('Nom d\'utilisateur ou mot de passe incorrect.');
+          setError({
+            api:'Nom d\'utilisateur ou mot de passe incorrect.'}
+          );
         }
       }
     } catch (error) {
       console.error('Erreur lors de la connexion :', error);
-      setError('Erreur lors de la connexion.');
+      setError({
+        api: 'Erreur lors de la connexion.'});
     } finally {
       setIsLoading(false);
     }
+  }
   };
+
+  const validForm = () => {
+    let errors = {};
+    if (!email) {
+      errors.email = "Email requis";
+    }
+    if (!password) {
+      errors.password = "Le mot de passe est requis";
+    }
+    setError(errors);
+    return Object.keys(errors).length === 0;
+  }
 
   const handleResendEmail = async (e) => {
     e.preventDefault();
     setIsResendingEmail(true); 
     try {
-      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}api/users/send-email-verification/${username}`, {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}api/users/send-email-verification/${email}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -90,10 +113,13 @@ const LoginPage = () => {
         });
         setError('');
       } else {
-        setError("Erreur lors de l'envoi du mail.");
+        setError({
+          apiMail: "Erreur lors de l'envoi du mail."
+      });
       }
     } catch (error) {
-      setError("Erreur lors de l'envoi du mail.");
+      setError({
+        apiMail: "Erreur lors de l'envoi du mail."});
     } finally {
       setIsResendingEmail(false); 
     }
@@ -108,39 +134,59 @@ const LoginPage = () => {
             <FormLabel>Email</FormLabel>
             <Input
               type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
+            {error.email && <Text color="red.500" mb={4} pt={1}>
+            {error.email}
+          </Text> }
+
           </FormControl>
           <FormControl>
             <FormLabel>Mot de passe</FormLabel>
+            <InputGroup size='md'>
             <Input
-              type="password"
+              type={show ? 'text' : 'password'}
+              placeholder='Mot de passe'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
+
+            <InputRightElement width='4.5rem'>
+            {show ? <BiShowAlt onClick={()=> {setShow(!show)}}/> : <BiHide onClick={()=> {setShow(!show)}}/>}
+            </InputRightElement>  
+            </InputGroup>
+            
+            {error.password && <Text color="red.500" mb={4} pt={1}>
+            {error.password}
+            </Text> }
+          
           </FormControl>
           <Link to="/auth/forgetpassword">
             <Button mt={4}>Mot de passe oublié</Button>
           </Link>
-          <Button type="submit" colorScheme="blue" size="lg" disabled={isLoading}>
+          <Button type="submit" bg="black" size="lg" color="white">
             {isLoading ? <Spinner size="sm" color="white" /> : "Se connecter"}
           </Button>
         </Stack>
       </form>
-      {error && (
+      {error.api && (
         <Text color="red.500" mb={4} textAlign="center">
-          {error}
-          {error === 'Votre email n\'a pas encore été validé. Veuillez vérifier votre boîte de réception et valider votre compte.' && (
+          {error.api}
+          {error.api === 'Votre email n\'a pas encore été validé. Veuillez vérifier votre boîte de réception et valider votre compte.' && (
             <Button variant="link" onClick={handleResendEmail} disabled={isResendingEmail}>
               {isResendingEmail ? <Spinner size="sm" color="blue.500" /> : "Renvoyer l'email de validation"}
             </Button>
           )}
         </Text>
       )}
+      <Text textAlign="center" mt={4}>
+        Pas encore de compte ?
+      </Text>
       <Link to="/auth/login">
-            <Button mt={4}>Créer un compte</Button>
-          </Link>
+        <Button size="lg" mt={4}>Créer un compte</Button>
+      </Link>
     </Box>
   );
 };
