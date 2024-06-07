bbb64dedd19eed87212c53be4ebe5cb06ec90abb
jugo92 <jugzek92@outlook.fr>
2024-06-07 12:52:10 +0200

    throw exception if user is not found

--- server/src/Service/SecurityService.php
+++ server/src/Service/SecurityService.php
@@ -4,18 +4,19 @@ namespace App\Service;
 
 use Symfony\Component\Security\Core\Security;
 use App\Entity\User;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class SecurityService {
     public function __construct(private Security $security) {
         $this->security = $security;
     }
 
-    public function securityToken(): User|null
+    public function securityToken(): User|null 
     {
         $user = $this->security->getUser();
 
         if (!$user) {
-            return null;
+            throw new AccessDeniedHttpException('User is not validated');
         }
         return $user;
     }
