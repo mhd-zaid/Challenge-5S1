79af244a1376dfca4f81541a65dc9b8cc6e9a024
jugo92 <jugzek92@outlook.fr>
2024-06-14 14:49:11 +0200

    add group + remove useless properties

--- server/src/Entity/Studio.php
+++ server/src/Entity/Studio.php
@@ -32,7 +32,7 @@ class Studio
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['studio:read'])]
+    #[Groups(['studio:read', 'company:read', 'planning:read'])]
     #[Assert\NotBlank]
     #[Assert\Length(min:5,max: 255)]
     private ?string $name = null;
