4b9a79defd13c5f6c5ac1ba81a151b92eed0b39d
jugo92 <jugzek92@outlook.fr>
2024-06-15 12:54:58 +0200

    add validator form

--- client/src/components/forms/NewUnavailabilityHourForm.jsx
+++ client/src/components/forms/NewUnavailabilityHourForm.jsx
@@ -1,58 +1,65 @@
 import React, { useState } from 'react';
-import { FormControl, FormLabel, Input, Button, Select, Spinner, Box  } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { FormControl, FormLabel, Input, Button, Select, Spinner, Box, FormErrorMessage } from '@chakra-ui/react';
 import dayjs from 'dayjs';
 
 const NewUnavailabilityHourForm = ({ onSubmit, users, user }) => {
+  const { register, handleSubmit, reset, formState: { errors, isSubmitting }, setError, clearErrors } = useForm();
+  const [isSubmittingForm, setIsSubmittingForm] = useState(false);
 
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [selectedUser, setSelectedUser] = useState('');
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const onSubmitForm = async (formData) => {
+    if (dayjs(formData.startDate).isAfter(dayjs(formData.endDate))) {
+      setError('endDate', { type: 'manual', message: 'La date de fin doit être après la date de début' });
+      return;
+    }
+    
+    setIsSubmittingForm(true);
 
-  const handleSubmit = async () => {
-
-    setIsSubmitting(true);
-
-    const formData = {
-      startTime: startDate,
-      endTime: endDate,
-      employee: user.roles.includes('ROLE_PRESTA') ? selectedUser : `/api/users/${user.id}`,
+    const formValues = {
+      startTime: formData.startDate,
+      endTime: formData.endDate,
+      employee: user.roles.includes('ROLE_PRESTA') ? formData.selectedUser : `/api/users/${user.id}`,
       status: user.roles.includes('ROLE_PRESTA') ? 'Accepted' : 'Pending',
     };
-   await onSubmit(formData);
 
-   setStartDate('');
-   setEndDate('');
-   setSelectedUser('');
-   setIsSubmitting(false);
+    console.log(formValues);
+    await onSubmit(formValues);
+    reset();
+    setIsSubmittingForm(false);
+    clearErrors();
   };
 
   return (
-    <>
-      <FormControl mb="4">
+    <form onSubmit={handleSubmit(onSubmitForm)}>
+      <FormControl mb="4" isInvalid={errors.startDate}>
         <FormLabel>Début de l'absence</FormLabel>
         <Input
           type="datetime-local"
-          value={startDate}
-          onChange={(e) => setStartDate(e.target.value)}
+          {...register('startDate', { required: 'La date de début est requise' })}
           placeholder="Select start date"
         />
+        <FormErrorMessage>
+          {errors.startDate && errors.startDate.message}
+        </FormErrorMessage>
       </FormControl>
-      <FormControl mb="4">
+
+      <FormControl mb="4" isInvalid={errors.endDate}>
         <FormLabel>Fin de l'absence</FormLabel>
         <Input
           type="datetime-local"
-          value={endDate}
-          onChange={(e) => setEndDate(e.target.value)}
+          {...register('endDate', { required: 'La date de fin est requise' })}
           placeholder="Select end date"
         />
+        <FormErrorMessage>
+          {errors.endDate && errors.endDate.message}
+        </FormErrorMessage>
       </FormControl>
+
       {user.roles.includes('ROLE_PRESTA') && (
-        <FormControl mb="4">
+        <FormControl mb="4" isInvalid={errors.selectedUser}>
           <FormLabel>Utilisateur</FormLabel>
           <Select
-            value={selectedUser}
-            onChange={(e) => setSelectedUser(e.target.value)}
+            {...register('selectedUser', { required: 'L\'utilisateur est requis' })}
             placeholder="Sélectionnez un utilisateur"
           >
             {users.map((user) => (
@@ -61,12 +68,16 @@ const NewUnavailabilityHourForm = ({ onSubmit, users, user }) => {
               </option>
             ))}
           </Select>
+          <FormErrorMessage>
+            {errors.selectedUser && errors.selectedUser.message}
+          </FormErrorMessage>
         </FormControl>
       )}
-      <Button colorScheme="teal" onClick={handleSubmit} disabled={isSubmitting}>
-      {isSubmitting ? <Spinner size="sm" /> : 'Soumettre'}
+
+      <Button colorScheme="teal" type="submit" isDisabled={isSubmitting || isSubmittingForm}>
+        {isSubmittingForm ? <Spinner size="sm" /> : 'Soumettre'}
       </Button>
-    </>
+    </form>
   );
 };
 
