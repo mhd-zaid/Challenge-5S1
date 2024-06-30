dd64662b4a2cdbcb8ba7bb054c35aa2b2e8944ea
Zaid MOUHAMAD <zaidmouhamad@gmail.com>
2024-07-01 01:30:27 +0200

    stat admin

--- client/src/pages/profile/AdminProfilePage.jsx
+++ client/src/pages/profile/AdminProfilePage.jsx
@@ -20,6 +20,7 @@ import {
   Input,
 } from '@chakra-ui/react';
 import ManageAccount from '@/components/Modal/ManageAccount.jsx';
+import BarChartAdmin from '@/components/BarChartAdmin.jsx';
 
 const AdminProfile = ({user}) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -62,6 +63,9 @@ const AdminProfile = ({user}) => {
         </Button>
       </Flex>
       <Stack spacing={4}>
+        <Box m={4} p={4} bg="white" shadow="md" borderRadius="md">
+          <BarChartAdmin />
+        </Box>
         <Box p={4} bg="white" shadow="md" borderRadius="md">
           <Heading as="h2" size="md" mb={2}>
             Informations générales
