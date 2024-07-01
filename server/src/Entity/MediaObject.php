b3ad1ea0fa6804da66854c58058c4b3ffe7e5fc4
jugo92 <jugzek92@outlook.fr>
2024-07-02 01:17:27 +0200

    remove operations

--- server/src/Entity/MediaObject.php
+++ server/src/Entity/MediaObject.php
@@ -1,17 +1,9 @@
 <?php
-// api/src/Entity/MediaObject.php
-
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiProperty;
-use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\Get;
-use ApiPlatform\Metadata\GetCollection;
-use ApiPlatform\Metadata\Post;
-use ApiPlatform\OpenApi\Model;
 use App\Entity\Traits\BlameableTrait;
 use App\Entity\Traits\TimestampableTrait;
-use App\State\SaveMediaObject;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -20,38 +12,6 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[Vich\Uploadable]
 #[ORM\Entity]
-#[ApiResource(
-    types: ['https://schema.org/MediaObject'],
-    operations: [
-        new Get(),
-        new GetCollection(),
-        new Post(
-            openapi: new Model\Operation(
-                requestBody: new Model\RequestBody(
-                    content: new \ArrayObject([
-                        'multipart/form-data' => [
-                            'schema' => [
-                                'type' => 'object',
-                                'properties' => [
-                                    'file' => [
-                                        'type' => 'string',
-                                        'format' => 'binary'
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ])
-                )
-            ),
-            validationContext: ['groups' => ['Default', 'media_object_create']],
-            deserialize: false,
-            processor: SaveMediaObject::class
-        )
-    ],
-    outputFormats: ['json' => ['application/json']],
-    normalizationContext: ['groups' => ['media_object:read']],
-
-)]
 class MediaObject
 {
     use TimestampableTrait;
@@ -61,7 +21,6 @@ class MediaObject
     private ?int $id = null;
 
     #[ApiProperty(types: ['https://schema.org/contentUrl'])]
-    #[Groups(['media_object:read', 'company:read', 'company:read:common'])]
     public ?string $contentUrl = null;
 
     #[Vich\UploadableField(mapping: 'media_object', fileNameProperty: 'filePath')]
