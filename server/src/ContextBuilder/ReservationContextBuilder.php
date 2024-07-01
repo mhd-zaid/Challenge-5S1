1b301cb54ebdedbe35452b1a205571ea129212af
jugo92 <jugzek92@outlook.fr>
2024-07-01 23:46:20 +0200

    groups reservation

---
+++ server/src/ContextBuilder/ReservationContextBuilder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\ContextBuilder;
+
+use ApiPlatform\Serializer\SerializerContextBuilderInterface;
+use App\Entity\Reservation;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+final class UserContextBuilder implements SerializerContextBuilderInterface
+{
+
+    public function __construct(
+        private SerializerContextBuilderInterface $decorated
+        , private AuthorizationCheckerInterface $authorizationChecker
+        , private Security $security
+    )
+    {}
+
+    public function createFromRequest(Request $request, bool $normalization, ?array $extractedAttributes = null): array
+    {
+        $context = $this->decorated->createFromRequest($request, $normalization, $extractedAttributes);
+        $resourceClass = $context['resource_class'] ?? null;
+
+        if (($resourceClass === Reservation::class) && isset($context['groups']) && $context['method']->getMethod() === 'PATCH'){
+                $context['groups'][] = 'reservation:update';
+        }
+        return $context;
+    }
+}
