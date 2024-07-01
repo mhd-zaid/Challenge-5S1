9126b286caab9a931932ad0afcc33fcdeb810272
jugo92 <jugzek92@outlook.fr>
2024-07-01 21:47:15 +0200

    groups Studio

--- server/src/Entity/Company.php
+++ server/src/Entity/Company.php
@@ -8,7 +8,6 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
-use App\Operation\SoftDelete;
 use App\Repository\CompanyRepository;
 use App\State\CompanyPostStateProcessor;
 use Doctrine\Common\Collections\ArrayCollection;
